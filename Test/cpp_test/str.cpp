03905cb4b9afbb570fa0b0d6dd39ec43ff4a3759
Augenstern-org <snowieaugenstern@gmail.com>
2025-12-15 21:55:38 +0800

    Dec15 写点别的吧，比如梳理一下思路

--- Test/cpp_test/str.cpp
+++ Test/cpp_test/str.cpp
@@ -1,7 +1,4 @@
-char str[]{"123"};
-
-char str1[] = "123";
-
-char* str2 = "123";
-
-str[0] = 'q';
+// str 是一个 char* 类型的指针，指向只读内存区的一个 const char[4] 对象
+char* str = "xxx";
+// str2 是一个 char[5] 类型的指针，指向一个 char[5] 类型的对象
+char str2[] = "xxxx";
