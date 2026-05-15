aab996386c3adb8b73791e76a3290db230924ba7
Augenstern-org <snowieaugenstern@gmail.com>
2026-05-15 21:14:19 +0800

    呃呃，明天写快排吧。学的东西有点多，需要消化一下。

--- algorithm_prac/course/sort/merge_sort_iterator.cpp
+++ algorithm_prac/course/sort/merge_sort_iterator.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cstring>
 #define MAXA 1024
 
 void merge(const int* aBegin, const int* aEnd, const int* bBegin,
@@ -17,8 +19,15 @@ void merge(const int* aBegin, const int* aEnd, const int* bBegin,
 }
 
 void sort(int* arr, int len) {
-    int size = 1;
-    while (size <= len) {
-        //
+    int* temp = new int[len];
+    for (int size = 1; size < len; size *= 2) {
+        for (int left = 0; size < len; left += size * 2) {
+            int mid = std::min(left + size, len);
+            int right = std::min(left + 2 * size, len);
+            if (mid >= len) break;
+            merge(arr + left, arr + mid, arr + mid, arr + right, temp + left);
+        }
+        std::memcpy(arr, temp, len * sizeof(int));
     }
+    delete[] temp;
 }
